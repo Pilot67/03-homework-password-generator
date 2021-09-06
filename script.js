497a63074c51fe18fd1b9381b9af1aa6082a55fa
Stuart Simmons <stuart@Stuart.local>
2021-09-07 09:29:48 +1000

    Refined js code

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,8 +11,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
 
 function generatePassword(){
   // declare some local variables
@@ -33,64 +27,49 @@ function generatePassword(){
     numericalChars: ['0','1','2','3','4','5','6','7','8','9'],
     specialChars: ['@','%','+',"\\",'/',"\'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.']
   }
-  
+  //generatedPassword variable  
   var generatedPassword = '';
 
   // Prompt for the password length and check if it is between min and max - loop if its outside of the parameters
-  do{ 
+    do{ 
     pwdParameters.length = parseInt(prompt("How long would you like your password \nIt must be between "
     + pwdParameters.minLength + " and " + pwdParameters.maxLength + " characters"));
 
-    if (pwdParameters.minLength <= pwdParameters.length && pwdParameters.length <= pwdParameters.maxLength){
-      alert("Correct Length of " + pwdParameters.length);
-      correctLength = true;
-    }else if (isNaN(pwdParameters.length)){ //if cancel is pressed or letters entered
-      alert("You must use a number between "+ pwdParameters.minLength + " and " + pwdParameters.maxLength + " characters");
-      return null;
-    }else{ // length is outside of set parameters
-      alert("Incorrect length\nThe length must be between "+ pwdParameters.minLength + " and " + pwdParameters.maxLength + " characters");
-      correctLength = false;
-    }
-  }while (! correctLength);
+      if (pwdParameters.minLength <= pwdParameters.length && pwdParameters.length <= pwdParameters.maxLength){
+        alert("You have selected a password lengh of " + pwdParameters.length);
+        correctLength = true;
+      }else if (isNaN(pwdParameters.length)){ //if cancel is pressed or letters entered
+        alert("You must use a number between "+ pwdParameters.minLength + " and " + pwdParameters.maxLength + " characters");
+        return null;
+      }else{ // length is outside of set parameters
+        alert("Incorrect length\nThe length must be between "+ pwdParameters.minLength + " and " + pwdParameters.maxLength + " characters");
+        correctLength = false;
+      }
+    }while (! correctLength);
 
-  if (pwdParameters.useLowercase = confirm("Would you like to use lowercase charaters")) {
-    usableChars.usableCharArray = usableChars.usableCharArray.concat(usableChars.lowerCaseChars);
-    alert("Lower case selected");
-  }
-  if (pwdParameters.useUppercase = confirm("Would you like to use UPPERCASE charaters")) {
-    usableChars.usableCharArray = usableChars.usableCharArray.concat(usableChars.upperCaseChars);
-    alert("UPPER case selected");
-  }
-  if (pwdParameters.useNumbers = confirm("Would you like to use numbers charaters")) {
-    usableChars.usableCharArray = usableChars.usableCharArray.concat(usableChars.numericalChars);
-    alert("Numbers selected");
-  }
-  if (pwdParameters.useSpecial = confirm("Would you like to use special charaters charaters")) {
-    usableChars.usableCharArray = usableChars.usableCharArray.concat(usableChars.specialChars);
-    alert("Special charaters selected");
-  }
-  // Check at least 1 set of chareters has been selected
-  if (!(pwdParameters.useLowercase || pwdParameters.useUppercase || pwdParameters.useNumbers || pwdParameters.useSpecial)) {
-    alert("Nothing selected\nYou must choose at least 1 set of charaters");
-    return null;
-  }
-  
-  // if (pwdParameters.useLowercase){
-  //   usableChars.usableCharArray = usableChars.usableCharArray.concat(usableChars.lowerCaseChars);
-  // };
-  // if (pwdParameters.useUppercase){
-  //   usableChars.usableCharArray = usableChars.usableCharArray.concat(usableChars.upperCaseChars);
-  // };
-  // if (pwdParameters.useNumbers){
-  //   usableChars.usableCharArray = usableChars.usableCharArray.concat(usableChars.numericalChars);
-  // };
-  // if (pwdParameters.useSpecial){
-  //   usableChars.usableCharArray = usableChars.usableCharArray.concat(usableChars.specialChars);
-  // };
-  
-  for (i=0; i<pwdParameters.length; i++) {
-    char = Math.floor(Math.random()*usableChars.usableCharArray.length);
-    generatedPassword = generatedPassword + usableChars.usableCharArray[char];
-  }
+    //using window prompts confirm the use of lower, upper, numbers and special characters
+    if (pwdParameters.useLowercase = confirm("Would you like to use lowercase charaters")) {
+      usableChars.usableCharArray = usableChars.usableCharArray.concat(usableChars.lowerCaseChars);
+    }
+    if (pwdParameters.useUppercase = confirm("Would you like to use UPPERCASE charaters")) {
+      usableChars.usableCharArray = usableChars.usableCharArray.concat(usableChars.upperCaseChars);
+    }
+    if (pwdParameters.useNumbers = confirm("Would you like to use numbers charaters")) {
+      usableChars.usableCharArray = usableChars.usableCharArray.concat(usableChars.numericalChars);
+    }
+    if (pwdParameters.useSpecial = confirm("Would you like to use special charaters charaters")) {
+      usableChars.usableCharArray = usableChars.usableCharArray.concat(usableChars.specialChars);
+    }
+    // Check at least 1 set of chareters has been selected
+    if (!(pwdParameters.useLowercase || pwdParameters.useUppercase || pwdParameters.useNumbers || pwdParameters.useSpecial)) {
+      alert("Nothing selected\nYou must choose at least 1 set of charaters");
+      return null;
+    }
+    
+    //for loop to generate the password
+    for (i=0; i<pwdParameters.length; i++) {
+      char = Math.floor(Math.random()*usableChars.usableCharArray.length);
+      generatedPassword += usableChars.usableCharArray[char];
+    }
   return generatedPassword;
 }// End of function
